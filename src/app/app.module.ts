ea3cf5ada6efe31fb340ca2dd805b4e69410407b
Fabian Ramirez <fabianrm@gmail.com>
2022-02-05 15:08:35 -0500

    Creacion de editar y listar empleados componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgregarEmpleadoComponent
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent
   ],
   imports: [
     BrowserModule,
